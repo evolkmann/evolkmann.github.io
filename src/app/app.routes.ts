58066ef72b5695cc84e2fb1a639de96d83a177ae
Enzo Volkmann <enzo@volkmann.dev>
2024-07-09 09:51:30 +0200

    remove angular app

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TodoListComponent } from './todos/views/todo-list/todo-list.component';
-
-export const routes: Routes = [
-  {
-    path: 'todos',
-    component: TodoListComponent
-  }
-];
